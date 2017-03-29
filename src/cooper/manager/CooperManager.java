3005bfa47348c1430984192613e68bc19680e412
jonatanlara <jonatan2332@hotmail.com>
2017-03-29 00:24:56 -0600

    primer coommit

---
+++ src/cooper/manager/CooperManager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cooper.manager;
+
+import GUI.Login;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CooperManager {
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
